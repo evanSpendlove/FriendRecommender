7ab5e3bfa0455fe865a3db09cfcb2b7cb2b48587
evanSpendlove <evan.spendlove@ucdconnect.ie>
2019-01-25 11:49:34 +0000

    Added typdef struct

--- attempt_1.c
+++ attempt_1.c
@@ -2,6 +2,18 @@
 
 #include <stdio.h>
 
+/* 
+    Defines a new structure, fb_user, in which I store 
+    each user and up to (inclusive) 4 of their friends.
+    It also includes the total number of their friends.
+*/
+typedef struct fb_user
+{
+    char username[100];
+    char friend[4][100];
+    int friend_count;
+} fb_user;
+
 int main(void)
 {
     
